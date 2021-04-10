b247e153def962ca293a3c9d0e933a7c10373c01
Retroboy <naskin.iv@gmail.com>
2021-04-10 20:41:47 +0300

    refactor "rtc_ds3231"

--- Software/Power_bank/RTC_DS3231.c
+++ Software/Power_bank/RTC_DS3231.c
@@ -1,95 +1,59 @@
-#include "RTC_DS3231.h"
+#include "rtc_ds3231.h"
 
-#define SEC_REG		0x00
-#define MIN_REG		0x01
-#define HOUR_REG	0x02
-#define CNTRL_REG	0x0E
-#define STATUS_REG	0x0F
-#define A1M1_BIT	0x07
-
-static uint8_t RTC_wr_addr = 0b11010000;
-static uint8_t RTC_read_addr = 0b11010001;
-
-uint8_t sec, min, hour;
-
+/*Utilities*/
 static uint8_t dec_to_bin(uint8_t c)
 {
-	uint8_t ch = ((c/10)<<4)|(c%10);
+	uint8_t ch = ((c / 10)<<4 | c % 10);
 	return ch;
 }
 
 static uint8_t bin_to_dec(uint8_t c)
 {
-	uint8_t ch = ((c>>4)*10+(0b00001111&c));
+	uint8_t ch = ((c>>4) * 10 + (0b00001111&c));
 	return ch;
 }
+/*RTC*/
+static void rtc_sqw_set(void);
 
-void RTC_init(void)
+void rtc_init(void)
 {
 	i2c_start();
-	i2c_sendbyte(RTC_wr_addr);
-	i2c_sendbyte(CNTRL_REG);
-	i2c_sendbyte(0b00000000);				//Enable 1Hz wave
+	i2c_sendbyte(DS3231_WRITE_ADDR);
+	i2c_sendbyte(DS3231_STATUS_ADDR);
+	i2c_sendbyte(DS3231_STATUS_EN32KHZ);
+	i2c_stop();	
+	rtc_sqw_set();	
+}
+
+void rtc_set_time(Time_t *t, uint8_t hour, uint8_t min)
+{
+	i2c_start();
+	i2c_sendbyte(DS3231_WRITE_ADDR);	
+	i2c_sendbyte(DS3231_TIME_MIN_ADDR);
+	i2c_sendbyte(dec_to_bin(t->min));
+	i2c_sendbyte(dec_to_bin(t->hour));
 	i2c_stop();
 }
 
-void RTC_set_time(uint8_t hour, uint8_t min, uint8_t sec)
-{
+void rtc_get_time(Time_t *t)
+{	
 	i2c_start();
-	i2c_sendbyte(RTC_wr_addr);
-	i2c_sendbyte(SEC_REG);
-	i2c_sendbyte(dec_to_bin(sec));				
-	i2c_sendbyte(dec_to_bin(min));
-	i2c_sendbyte(dec_to_bin(hour));
-	i2c_stop();		
-}
-
-void RTC_read_time(void)
-{
-	i2c_start();
-	i2c_sendbyte(RTC_wr_addr);
-	i2c_sendbyte(SEC_REG);
+	i2c_sendbyte(DS3231_WRITE_ADDR);
+	i2c_sendbyte(DS3231_TIME_CAL_ADDR);
 	i2c_stop();
 	i2c_start();
-	i2c_sendbyte(RTC_read_addr);
-	sec = i2c_readbyte();
-	min = i2c_readbyte();
-	hour = i2c_readbyte();
-	sec = bin_to_dec(sec);
-	min = bin_to_dec(min);
-	hour = bin_to_dec(hour);
-
+	i2c_sendbyte(DS3231_READ_ADDR);
+	t->sec = i2c_readbyte();
+	t->min = i2c_readbyte();
+	t->hour = i2c_readbyte();
 }
 
-void RTC_SQW_set(void)
+static void rtc_sqw_set(void)
 {
 	i2c_start();
-	i2c_sendbyte(RTC_wr_addr);
-	i2c_sendbyte(CNTRL_REG);
-	i2c_sendbyte(0x00);				//bits RS0 RS1 INTCN in 0
-	i2c_stop();
-}
-
-void RTC_hour_increment(void)
-{
-	i2c_start();
-	i2c_sendbyte(RTC_wr_addr);
-	i2c_sendbyte(HOUR_REG);
-	if(hour < 23)
-		i2c_sendbyte(dec_to_bin(hour++));
-	else
-		i2c_sendbyte(0);
-	i2c_stop();
-}
-
-void RTC_min_increment(void)
-{
-	i2c_start();
-	i2c_sendbyte(RTC_wr_addr);
-	i2c_sendbyte(MIN_REG);
-	if(min < 59)
-		i2c_sendbyte(dec_to_bin(min++));
-	else
-		i2c_sendbyte(0);
+	i2c_sendbyte(DS3231_WRITE_ADDR);
+	i2c_sendbyte(DS3231_CONTROL_ADDR);
+	/*Bits RS0 RS1 to LOW*/
+	i2c_sendbyte(0x00);					
 	i2c_stop();
 }
--- Software/Power_bank/RTC_DS3231.h
+++ Software/Power_bank/RTC_DS3231.h
@@ -3,14 +3,48 @@
 
 #include "main.h"
 
+typedef struct
+{
+	uint8_t sec;
+	uint8_t min;
+	uint8_t hour;
+} Time_t;
 
-void RTC_init(void);
-void RTC_set_time(uint8_t hour, uint8_t min, uint8_t sec);
-void RTC_read_time(void);
-void RTC_SQW_set(void);
-void RTC_hour_increment(void);
-void RTC_min_increment(void);
+void rtc_init(void);
+void rtc_get_time(Time_t *time);
+void rtc_set_time(Time_t *time, uint8_t hour, uint8_t min);
+
+#define DS3231_WRITE_ADDR           0x68
+#define DS3231_READ_ADDR            0x69
+
+/*REGISTERS*/
+/*Time*/
+#define DS3231_TIME_CAL_ADDR        0x00
+#define DS3231_TIME_MIN_ADDR        0x01
+#define DS3231_TIME_HOUR_ADDR       0x02
+#define DS3231_ALARM1_ADDR          0x07
+#define DS3231_ALARM2_ADDR          0x0B
+#define DS3231_CONTROL_ADDR         0x0E
+#define DS3231_STATUS_ADDR          0x0F
+#define DS3231_AGING_OFFSET_ADDR    0x10
+#define DS3231_TEMPERATURE_ADDR     0x11
+
+/*Control*/
+#define DS3231_CONTROL_A1IE			0x1		/* Alarm 2 Interrupt Enable */
+#define DS3231_CONTROL_A2IE			0x2		/* Alarm 2 Interrupt Enable */
+#define DS3231_CONTROL_INTCN		0x4		/* Interrupt Control */
+#define DS3231_CONTROL_RS1			0x8		/* square-wave rate select 2 */
+#define DS3231_CONTROL_RS2    		0x10	/* square-wave rate select 2 */
+#define DS3231_CONTROL_CONV			0x20	/* Convert Temperature */
+#define DS3231_CONTROL_BBSQW		0x40	/* Battery-Backed Square-Wave Enable */
+#define DS3231_CONTROL_EOSC			0x80	/* not Enable Oscillator, 0 equal on */
+
+/*Status*/
+#define DS3231_STATUS_A1F			0x01		/* Alarm 1 Flag */
+#define DS3231_STATUS_A2F			0x02		/* Alarm 2 Flag */
+#define DS3231_STATUS_BUSY			0x04		/* device is busy executing TCXO */
+#define DS3231_STATUS_EN32KHZ		0x08		/* Enable 32KHz Output  */
+#define DS3231_STATUS_OSF			0x80		/* Oscillator Stop Flag */
 
 
-
-#endif /* RTC_DS3231_H_ */
+#endif /* rtc_ds3231_H_ */
