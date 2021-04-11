511479d222da575e709cd1a4ea20ce6252bb8f6f
Retroboy <naskin.iv@gmail.com>
2021-04-11 12:54:47 +0300

    update acs712

--- Software/Power_bank/acs712.c
+++ Software/Power_bank/acs712.c
@@ -1,18 +1,35 @@
 #include "acs712.h"
 
+/*
+	ACS712 is analog hall-based current sensor IC.
+	It output signal is linear changing according to current value
+	Idle output signal is VCC/2. Sensitivity is 0.185V/A
+*/
+
+#define MAX_COUNTS		100
+
+static const float Sensitivity = 0.185;
+static const float Offset = 0;
+static const float Vref = 5.093;
+
 static uint8_t meas_num;
 static uint8_t adc_values[MAX_COUNTS];
-float adc_average;
+static float adc_average;
 
 void adc_init(void)
 {
+	/*Enable ADC(ADEN) and Interrupts(ADIE)*/
 	ADCSRA |= 1<<ADEN | 1<<ADIE
+	/*Division factor is 128*/
 		   |  1<<ADPS0 | 1<<ADPS1 | 1<<ADPS2;
-	ADMUX  |= 1<<ADLAR | 1<<REFS0 | 1<<MUX1; //ADC2						
+	/*Left adjust result(ADLAR) AVCC is Vref(REFS0) adc idle channel is ADC0*/
+	ADMUX  |= 1<<ADLAR | 1<<REFS0;
 }
 
 void adc_start(void)
 {
+	/*Switch between ACS712 at input and output*/
+	
 	if(oled_show_mode == MODEINPUT)
 	{
 		ADMUX  |= 1<<MUX0;
@@ -23,21 +40,13 @@ void adc_start(void)
 		ADMUX  |= 1<<MUX1;
 		ADMUX  &= ~(1<<MUX0);
 	}
-	ADCSRA |= (1<<ADSC);
+	/*Start next conversion(ADSC)*/
+	ADCSRA |= 1<<ADSC;
 }
 
-float acs712_get_current(void)
-{
-	float voltage, current;
-	
-	voltage = (VREF * adc_average / 255) - OFFSET;
-	current = (voltage - VREF / 2) / ACS712_SENSITIVITY5;
-	if(current < 0)
-		current *= -1;
-	
-	return current;
-} 
-
+/*
+	Get average value of internal adc of MAX_COUNTS measurements	
+*/
 ISR(ADC_vect)
 {
 	if(meas_num != MAX_COUNTS)
@@ -47,11 +56,27 @@ ISR(ADC_vect)
 	}
 	else
 	{
-		uint16_t temp = 0;
+		float sum = 0;
 		for(uint8_t i = 0; i < MAX_COUNTS; i++)
-			temp += adc_values[i];
-		adc_average = (float)temp / (float)MAX_COUNTS;
+			sum += adc_values[i];
+		adc_average = sum / MAX_COUNTS;
 		meas_num = 0;
 	}
 	adc_start();
 }
+
+/*
+	Return averaged current value
+*/
+float get_current(void)
+{
+	float adc_voltage, current;
+	
+	adc_voltage = (Vref * adc_average / 255) - Offset;
+	current = (adc_voltage - Vref / 2) / Sensitivity;
+	/*get absolute value of 'current'*/
+	if(current < 0)
+		current *= -1;
+	
+	return current;
+} 
--- Software/Power_bank/acs712.h
+++ Software/Power_bank/acs712.h
@@ -5,13 +5,9 @@
 
 void adc_init(void);
 void adc_start(void);
-float acs712_get_current(void);
+float get_current(void);
 
-extern uint8_t oled_show_mode;
+extern enum Oled_show_mode oled_show_mode;
 
-#define ACS712_SENSITIVITY5 0.185
-#define OFFSET				0
-#define VREF				5.093
-#define MAX_COUNTS			100
 
 #endif /* ACS712_H_ */
